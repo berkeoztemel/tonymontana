b0a6ea413f837948073d2ddd161a9a2899d219fd
berkeoztemel <berke.oztemel@hotmail.com>
2024-10-06 21:47:50 -0700

    new update_10-16

---
+++ projects-berke/amazon/amazon_6_question.py
@@ -0,0 +1,16 @@
+class BasedOnCustomDeck:
+    '''Program in any language to arrange a deck of cards in below order.
+
+- Based on Color: Spade, Heart, Diamond, Club.
+- Based on number: Ace>King>Queen>Jack>Higher number >lower numbers.'''
+
+
+
+
+
+
+
+
+
+
+
--- projects-berke/amazon/main_code.py
+++ projects-berke/amazon/main_code.py
@@ -1,6 +1,7 @@
 
 from amazon_1_question import SegregateArray
 from amazon_2_question import AscendingOrder
+from amazon_5_question import DealCard
 from amazon_7_question import FizzBuzz
 
 
@@ -29,9 +30,11 @@ while is_on == True:
         numbers = list(map(int, input("Enter multiple values for ascending numbers:(ex: 1 3 9 2 .. ..): \n").split()))
         print(AscendingOrder.bubble_sort(numbers))
 
-    #need more information about question 3 and question 4
-    #question 5 is on progress
-    #question 6 not start yet
+
+    elif question_number == "5":
+        new_deck = DealCard.create_deck()
+        DealCard.shuffle_deck(new_deck)
+        print(DealCard.deal_card(new_deck))
 
     elif question_number == "7":
         numbers = int(input("Enter number for FizzBuzz Game: \n"))
